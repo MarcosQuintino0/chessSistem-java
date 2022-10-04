a9c9080d0810e4359484ced19ea10c40aa192070
Marcos andre quintino <marcosquintinodev@gmail.com>
2022-10-04 16:57:07 -0300

    Placing pieces on the board

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,13 +1,17 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
 	private Board board;
 	
 	public ChessMatch() {
-		board = new Board(8,8);
+		board = new Board(8,8); //na hora que foi criada minha partida eu crio um tabuleiro 8 por 8 e chamo meu initialSetup
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -18,6 +22,11 @@ public class ChessMatch {
 			}
 		}
 		return mat;
+	}
+	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+		board.placePiece(new King(board, Color.BLACK), new Position(0,4));
 	}
 	
 }
