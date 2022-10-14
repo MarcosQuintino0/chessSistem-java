d887ab15bf2709c1bad5d1d4f64059b7722977fc
Marcos andre quintino <marcosquintinodev@gmail.com>
2022-10-14 16:47:41 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if(chessMatch.getPromoted() !=null) {
+					System.out.print("Enter pece for promotion (B/N/R/Q):");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
